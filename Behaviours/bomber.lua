4f04250c8a05f17ebf258ebe78b9950c8e7fe343
Psychosquid <psychohovership@gmail.com>
2026-03-05 07:46:53 +0800

    Compacted the codes - Type-P

--- Behaviours/bomber.lua
+++ Behaviours/bomber.lua
@@ -1,22 +1,14 @@
 local mx = 0.3
 local timer = 0
 local trailTimer = 0
-
 local planeSprite = 0
-
 local mineSprite
 local minePeekX = -5
 local minePeekY = -20
-
+local firstLaunch = 30
 local firePattern
 local fireSFX
-
-local firstLaunch = 30
-
-local allowDmgFrames = false --
-
-
-
+local allowDamageFrames = false
 
 function OnInitialise()
     mineSprite = self.SpawnAttachedSpriteAnimator("Effects/Bullets/bullet bomb", -100, false)
@@ -27,74 +19,39 @@ function OnInitialise()
     firePattern = NewFirePatternFromEntityData(self.data)
 end
 
-
-
 function OnTick()
-
-
-    dmgSprite = self.GetDamageFrame(self.hitPoints / 1.75)   
-
-
-    if allowDmgFrames == false then
-        self.animator.AnimateTo(planeSprite)
-    else
-        self.animator.AnimateTo(dmgSprite);
-    end
-
-
-
-
+    local damageframe = self.GetDamageFrame(self.hitPoints / 1.75)   
+    if allowDamageFrames == false then self.animator.GoTo(planeSprite) else self.animator.GoTo(damageframe) end
 
     mineSprite.position = { x = minePeekX, y = minePeekY }
 
     if ShouldKillPlayerOnTouch() == true then
-        if timer >= 0 then
-            timer = timer - 1
-        end
-
-
-
+        if timer > 0 then timer = timer - 1 end
         if timer <= 0 and planeSprite < 4 then
             timer = 10
             planeSprite = planeSprite + 1
         elseif timer <= 0 and planeSprite >= 4 then
-            allowDmgFrames = true
-            planeSprite = 5
+            allowDamageFrames = true
+            planeSprite = planeSprite + 1
             mineSprite.Initialise("Effects/Bullets/bullet bomb", 0)
         end
-
-
-
-
-
     end
 
     self.movement = { x = mx, y = 0, z = 0 }
     mx = mx + 0.001
 
-    local smokePos = { x = self.worldPosition.x - 70, y = self.worldPosition.y + 3 }
-
     trailTimer = trailTimer - 1
     if trailTimer <= 0 then
         trailTimer = 16
-
         local smokeArgs = NewJSONObject()
-        local smokeTrail = 1
-
-        smokeArgs.AddFieldFloat("mx", smokeTrail)
-        SpawnEntityWorld("smokeRing3", smokePos, smokeArgs)
+        smokeArgs.AddFieldFloat("mx", 1)
+        SpawnEntityWorld("smokeRing3", { x = self.worldPosition.x - 70, y = self.worldPosition.y + 3 }, smokeArgs)
     end
 
     if CanFire() == true then
         firePattern.Tick()
-        if firstLaunch > 0 then
-            firstLaunch = firstLaunch - 1
-        end
-
-        if firePattern.GetTicksTillFire() == 25 or firstLaunch == 25 then
-            PlaySound(fireSFX)
-        end
-
+        if firstLaunch > 0 then firstLaunch = firstLaunch - 1 end
+        if firePattern.GetTicksTillFire() == 25 or firstLaunch == 25 then PlaySound(fireSFX) end
         if firePattern.GetTicksTillFire() <= 30 or firstLaunch <= 30 and firstLaunch > 0 then
             minePeekX = minePeekX - 1
             minePeekY = minePeekY - 3
@@ -105,16 +62,12 @@ function OnTick()
             minePeekX = -5
             minePeekY = -20
             mineSprite.position = { x = -5, y = -20 }
-
-            local minePos = { x = self.worldPosition.x - 35, y = self.worldPosition.y - 108 } -- og y pos - 65
-
             local mineArgs = NewJSONObject()
             mineArgs.AddFieldFloat("my", -3)
-
-            SpawnEntityWorld("enemyshot_bomb", minePos, mineArgs)
+            SpawnEntityWorld("enemyshot_bomb", { x = self.worldPosition.x - 35, y = self.worldPosition.y - 108 }, mineArgs)
         end
     end
-            
+
     if self.position.x > 840 then self.Deactivate() end
 end
 
@@ -124,13 +77,13 @@ function OnKill()
 end
 
 function CanFire()
-    return self.position.x >= 20
+    return self.position.x >= 15
 end
 
 function HasCollision()
     return true
 end
-
 function ShouldKillPlayerOnTouch()
-    return self.position.x >= -20
+    return self.position.x >= -25
 end
+
