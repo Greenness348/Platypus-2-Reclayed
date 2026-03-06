fb96d66c6077aedd8b58580cf4c87a74f6d7a43b
Psychosquid <psychohovership@gmail.com>
2026-03-06 18:10:48 +0800

    Update luaFlipPlane.lua using the new Lua functions.

--- Behaviours/luaFlipPlane.lua
+++ Behaviours/luaFlipPlane.lua
@@ -6,27 +6,50 @@ local timer = 82
 local flipTimer = 133
 local flipSprite = 0
 local spawnTurrets = false
-local turret1
-local turret2
+local turretEntity1
+local turretPosX1
+local turretPosY1
+local turretEntity2
+local turretPosX2
+local turretPosY2
 local allowDamageFrames = false
 
 function OnInitialise()
-    if self.commandArgs.HasField("topTurret") then turret1 = self.commandArgs.GetFieldString("topTurret") else turret1 = "turretFlipPlane1" end
-    if self.commandArgs.HasField("bottomTurret") then turret2 = self.commandArgs.GetFieldString("bottomTurret") else turret2 = "turretFlipPlane2" end
+    if self.customBehaviourData.HasField("topTurretEntity") then turretEntity1 = self.customBehaviourData.GetFieldString("topTurretEntity") else turretEntity1 = "turretFlipPlane1" end
+    if self.customBehaviourData.HasField("topTurretX") then turretPosX1 = self.customBehaviourData.GetFieldInt("topTurretX") else turretPosX1 = -22 end
+    if self.customBehaviourData.HasField("topTurretY") then turretPosY1 = self.customBehaviourData.GetFieldInt("topTurretY") else turretPosY1 =  67 end
+    if self.customBehaviourData.HasField("bottomTurretEntity") then turretEntity2 = self.customBehaviourData.GetFieldString("bottomTurretEntity") else turretEntity2 = "turretFlipPlane2" end
+    if self.customBehaviourData.HasField("bottomTurretX") then turretPosX2 = self.customBehaviourData.GetFieldInt("bottomTurretX") else turretPosX2 = -22 end
+    if self.customBehaviourData.HasField("bottomTurretY") then turretPosY2 = self.customBehaviourData.GetFieldInt("bottomTurretY") else turretPosY2 = -74 end
+    
     if self.commandArgs.HasField("fruit_set") then self.fruitSet = self.commandArgs.GetFieldInt("fruit_set") else self.fruitSet = 5 end
 end
 
 function OnTick()
     mxTotal = mx + knockback
-    if knockback > 0 then knockback = knockback - 0.065 else knockback = 0 end
+
+    if knockback > 0 then
+        knockback = knockback - 0.065
+    else
+        knockback = 0
+    end
+
     knockbackCap = mx + 1.3
 
     self.movement = { x = mxTotal, y = 0, z = 0 }
 
-    if timer > 0 then timer = timer - 1 end
-    if timer <= 0 and mx > -1.3 then mx = mx - 0.065 end
+    if timer > 0 then
+        timer = timer - 1
+    end
 
-    if flipSprite < 11 then flipTimer = flipTimer - 1 end
+    if timer <= 0 and mx > -1.3 then
+        mx = mx - 0.065
+    end
+
+    if flipSprite < 11 then
+        flipTimer = flipTimer - 1
+    end
+
     if flipSprite == 1 or flipSprite == 3 or flipSprite == 5 or flipSprite == 7 then
         flipSprite = flipSprite + 1
         flipTimer = 2
@@ -35,11 +58,16 @@ function OnTick()
     end
 
     local damageframe = self.GetDamageFrame(self.hitPoints / 3.25)   
-    if allowDamageFrames == false then self.animator.GoTo(flipSprite) else self.animator.GoTo(damageframe) end
+
+    if allowDamageFrames == false then
+        self.animator.AnimateTo(flipSprite)
+    else
+        self.animator.AnimateTo(damageframe);
+    end
 
     if spawnTurrets == false and flipSprite == 11 then
-        SpawnEntityChild(turret1, self, { x = -22, y = 67 }, NewJSONObject())
-        SpawnEntityChild(turret2, self, { x = -22, y = -74 }, NewJSONObject())
+        SpawnEntityChild(turretEntity1, self, { x = turretPosX1, y = turretPosY1 })
+        SpawnEntityChild(turretEntity2, self, { x = turretPosX2, y = turretPosY2 })
         allowDamageFrames = true
         spawnTurrets = true
     end
@@ -48,7 +76,9 @@ function OnTick()
 end
 
 function OnHitByBullet()
-    if mx <= 0.845 and self.position.x <= 770 then knockback = 2.145 - knockbackCap end
+    if mx <= 0.845 and self.position.x <= 770 then
+        knockback = 2.145 - knockbackCap
+    end
 end
 
 function OnKill()
