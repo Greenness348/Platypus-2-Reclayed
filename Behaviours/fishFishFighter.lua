632de7eefa3d7cffb4d8b0e5087c06caf0750ff8
Wolf2K <thewolfmaster2k@gmail.com>
2026-03-15 02:02:29 +0000

    Add files via upload

---
+++ Behaviours/fishFishFighter.lua
@@ -0,0 +1,96 @@
+local mx
+local my = 0
+local angle
+local target = nil
+local cooldownTimer
+local homingTimer
+
+local baseSpeed = 4      -- slightly slower than 5
+local homingSpeed = 6    -- faster when locking on
+
+function OnInitialise()
+    cooldownTimer = self.commandArgs.GetFieldFloat("homingCooldown", 50)
+    homingTimer = self.commandArgs.GetFieldFloat("homingTime", 150)
+
+    behind = self.commandArgs.GetFieldBool("isBehind", false)
+
+    if behind == false then
+        angle = 180
+    else
+        angle = 0
+    end
+end
+
+function OnTick()
+
+    if cooldownTimer > 0 then
+        cooldownTimer = cooldownTimer - 1
+    else
+        if target == nil then
+            target = GetRandomActivePlayer()
+        elseif target ~= nil and not target.isActive then
+            target = nil
+        end
+
+        if homingTimer > 0 then
+            homingTimer = homingTimer - 1
+
+            if target ~= nil then
+                local sourcePos = self.worldPosition
+                local targetPos = target.worldPosition
+
+                local targetAngle = math.deg(
+                    math.atan2(
+                        targetPos.y - sourcePos.y,
+                        targetPos.x - sourcePos.x
+                    )
+                )
+
+                angle = MoveTowardsAngle(angle, targetAngle, 4)
+            end
+        end
+    end
+
+    -- Choose speed depending on state
+    local speed = baseSpeed
+    if cooldownTimer <= 0 then
+        speed = homingSpeed
+    end
+
+    -- Calculate movement AFTER angle changes
+    local angleRad = math.rad(angle)
+    mx = math.cos(angleRad) * speed
+    my = math.sin(angleRad) * speed
+
+    self.movement = { x = mx, y = my, z = 0 }
+
+    -- Animation
+    local positiveAngle = (angle % 360 - 5.625) % 360
+    local animatorFrame = math.floor(
+        (positiveAngle / (360.0 / self.animator.totalFrames) +
+        self.animator.totalFrames/2) % self.animator.totalFrames
+    )
+    self.animator.GoTo(animatorFrame)
+
+    -- Bounds check
+    if cooldownTimer <= 0 then
+        if self.position.x > 800 or self.position.x < -200 then
+            self.Deactivate()
+        end
+        if self.position.y > 200 or self.position.y < -800 then
+            self.Deactivate()
+        end
+    end
+end
+
+function OnKill()
+    self.SpawnShipShards(10, -6, 0, -15, 5, 0, 0, 0, 0, 0, 0)
+end
+
+function HasCollision()
+    return self.position.x > -100 or cooldownTimer <= 0
+end
+
+function ShouldKillPlayerOnTouch()
+    return true
+end
