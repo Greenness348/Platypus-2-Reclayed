f3b8db338d0476100da129389d3e51670d862471
Psychosquid <psychohovership@gmail.com>
2026-03-26 14:03:00 +0800

    Fixed an error on icbmLaunched.lua

--- Behaviours/icbmLaunched.lua
+++ Behaviours/icbmLaunched.lua
@@ -63,8 +63,7 @@ function OnTick()
         self.Deactivate()
     end
         
-    if player1 == nil then player1 = GetPlayer(0) end
-    if player2 == nil then player2 = GetPlayer(1) end
+    player1 = GetPlayer(0)
     if player1.isActive then
         dx1 = player1.worldPosition.x - (self.worldPosition.x + (math.cos(math.rad(direction)) * 200))
         dy1 = player1.worldPosition.y - (self.worldPosition.y + (math.sin(math.rad(direction)) * 200))
@@ -73,13 +72,14 @@ function OnTick()
         dy1 = dy1 / length1
         if length1 < 150 then player1.TriggerWarning() end
     end
+    player2 = GetPlayer(1)
     if player2.isActive then
-        dx2 = player2.worldPosition.x - self.worldPosition.x
-        dy2 = player2.worldPosition.y - self.worldPosition.y
+        dx2 = player2.worldPosition.x - (self.worldPosition.x + (math.cos(math.rad(direction)) * 200))
+        dy2 = player2.worldPosition.y - (self.worldPosition.y + (math.sin(math.rad(direction)) * 200))
         length2 = math.sqrt(dx2 * dx2 + dy2 * dy2)
         dx2 = dx2 / length2
         dy2 = dy2 / length2
-        if length2 < 200 then player2.TriggerWarning() end
+        if length2 < 150 then player2.TriggerWarning() end
     end
 end
 
