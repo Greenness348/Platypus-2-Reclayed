fe04447259e5514c8e8c98e6c0948e80f1e7de05
Psychosquid <psychohovership@gmail.com>
2026-04-03 13:40:46 +0800

    Update deflectorShield.lua

--- Behaviours/deflectorShield.lua
+++ Behaviours/deflectorShield.lua
@@ -1,41 +1,39 @@
 local timer = 150
 local shieldTimer
-local sprite = 0
-local spriteTimer
-local activationSprite
-local shieldUp
+local shieldUpSprite
+local shieldUpAnimator
 local shieldSet = false
-local bullets
+local bulletCount
 local bulletEntity
 local deflectRadius
 local cooldownSet
 local cooldownTick = 0
 local minSpeed
 local maxSpeed
-local shards = 60
+local shardCount
+local shardEntity
 local activateSFX
 local disableSFX
-local deflectSFX
+local fireSFX
 
 function OnInitialise()
-    activationSprite = self.customBehaviourData.GetFieldString("activationSprite", "")
+    shieldUpSprite = self.customBehaviourData.GetFieldString("shieldUpSprite", "")
     bulletEntity = self.customBehaviourData.GetFieldString("bulletEntity", "")
+    shardEntity = self.customBehaviourData.GetFieldString("shardEntity", "")
     activateSFX = self.customBehaviourData.GetFieldString("activateSFX", "")
     disableSFX = self.customBehaviourData.GetFieldString("disableSFX", "")
-    deflectSFX = self.customBehaviourData.GetFieldString("deflectSFX", "")
+    fireSFX = self.customBehaviourData.GetFieldString("fireSFX", "")
     deflectRadius = self.customBehaviourData.GetFieldFloat("deflectRadius", 0)
+    shardCount = self.customBehaviourData.GetFieldInt("shardCount", 0)
 
-    shieldUp = self.SpawnAttachedSpriteAnimator(activationSprite, 1)
-    self.animator.Initialise(self.data.spriteName)
+    shieldUpAnimator = self.SpawnAttachedSpriteAnimator("empty", 1)
     self.animator.Initialise("empty")
-    shieldUp.Initialise("empty")
+    self.animator.ApplyLayerMaterial(self.layer)
 
     self.hitPoints = self.data.maxHitPoints + 1000
-    spriteTimer = timer - 28
-
     shieldTimer = NewDiffDictInt(250, 220, 180, 150, 120).Get()
     cooldownSet = NewDiffDictInt(10, 10, 5, 5, 2).Get()
-    bullets = NewDiffDictInt(2, 3, 3, 4, 4).Get()
+    bulletCount = NewDiffDictInt(2, 3, 3, 4, 4).Get()
     minSpeed = NewDiffDictFloat(2, 3, 4, 5, 6).Get()
     maxSpeed = NewDiffDictFloat(4, 6, 8, 10, 12).Get()
 end
@@ -45,24 +43,21 @@ function OnTick()
         if timer > 0 then timer = timer - 1
         else
             self.animator.Initialise(self.data.spriteName)
-            shieldUp.Initialise("empty")
+            self.animator.ApplyLayerMaterial(self.layer)
+            shieldUpAnimator.Initialise("empty")
+            shieldUpAnimator.ApplyLayerMaterial(self.layer)
             shieldSet = true
         end
         
-        if timer == 30 then
+        if timer == 28 then
             if activateSFX ~= "" then PlaySound(activateSFX) end
-            shieldUp.Initialise(activationSprite)
-        end
-        
-        if spriteTimer > 0 then spriteTimer = spriteTimer - 1
-        elseif spriteTimer <= 0 and sprite < 9 then
-            sprite = sprite + 1
-            spriteTimer = 2
+            if shieldUpAnimator ~= "" then shieldUpAnimator.Initialise(shieldUpSprite) end
+            shieldUpAnimator.ApplyLayerMaterial(self.layer)
         end
     end
     
-    shieldUp.AnimateTo(sprite, false)
-    self.animator.AnimateToNextFrame(true)
+    if timer <= 25 then shieldUpAnimator.AnimateToNextFrame(false) end
+    self.animator.LoopAnimation()
     
     if cooldownTick > 0 then cooldownTick = cooldownTick - 1 end
 
@@ -70,14 +65,13 @@ function OnTick()
         if disableSFX ~= "" then PlaySound(disableSFX) end
         self.hitPoints = self.data.maxHitPoints + 1000
         self.animator.Initialise("empty")
+        self.animator.ApplyLayerMaterial(self.layer)
         timer = shieldTimer
-        spriteTimer = shieldTimer - 28
-        sprite = 0
         shieldSet = false
-        for i = 0, shards - 1 do
+        for i = 0, shardCount - 1 do
             local ox = math.random(-deflectRadius, deflectRadius)
             local oy = math.random(-deflectRadius, deflectRadius)
-            SpawnEntityWorld("deflectorShieldShard", { x = self.worldPosition.x + ox, y = self.worldPosition.y + oy })
+            if shardEntity ~= "" then SpawnEntityWorld(shardEntity, { x = self.worldPosition.x + ox, y = self.worldPosition.y + oy }) end
         end            
     end
 end
@@ -86,8 +80,8 @@ function OnHitByBullet()
     if shieldSet == true then
         if cooldownTick <= 0 then
             cooldownTick = cooldownSet
-            if deflectSFX ~= "" then PlaySound(deflectSFX) end
-            for i = 0, bullets - 1 do
+            if fireSFX ~= "" then PlaySound(fireSFX) end
+            for i = 0, bulletCount - 1 do
                 local angleDeg = math.random(0, 360)
                 local angleRad = math.rad(angleDeg)
 
@@ -109,10 +103,10 @@ end
 function OnKill()
     if shieldSet == true or timer <= 30 then
         if disableSFX ~= "" then PlaySound(disableSFX) end
-        for i = 0, shards - 1 do
+        for i = 0, shardCount - 1 do
             local ox = math.random(-deflectRadius, deflectRadius)
             local oy = math.random(-deflectRadius, deflectRadius)
-            SpawnEntityWorld("deflectorShieldShard", { x = self.worldPosition.x + ox, y = self.worldPosition.y + oy })
+            if shardEntity ~= "" then SpawnEntityWorld(shardEntity, { x = self.worldPosition.x + ox, y = self.worldPosition.y + oy }) end
         end      
     end
 end
